b8f0301e28e582b68b74a2a10fcad0cb31c23c25
Peter Baumgartner <pete@lincolnloop.com>
2022-08-12 14:15:00 -0600

    wagtail start

---
+++ demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-z59%za0ibnyp2z&cr5j_w1#%j-v!5!%-th+d&c-h77fq7yw2_g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

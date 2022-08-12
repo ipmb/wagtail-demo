ffe1552e39a7b7d00b0bb500e4608280580b9637
Peter Baumgartner <pete@lincolnloop.com>
2022-08-12 14:27:24 -0600

    Production settings

--- demo/settings/production.py
+++ demo/settings/production.py
@@ -1,6 +1,11 @@
+import os
+
 from .base import *
 
 DEBUG = False
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 try:
     from .local import *
